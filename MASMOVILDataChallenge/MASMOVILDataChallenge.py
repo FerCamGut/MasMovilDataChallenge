73d2fefc6d15f5955abadea4a90f4107d494672d
Fernando C G <fercam2010@hotmail.com>
2021-09-30 11:07:42 +0200

    python 2)

--- MASMOVILDataChallenge/MASMOVILDataChallenge.py
+++ MASMOVILDataChallenge/MASMOVILDataChallenge.py
@@ -18,7 +18,16 @@ class Trabajador(Persona):
         # Nuevos atributos
         self.departamento = departamento
         self.puesto = puesto
+    
+    def presentation(self):
+        super().presentation()
+        print(f"Mi departamento es {self.departamento} y mi puesto es {self.puesto}")
+
 		
 nombre = 'Alberto'
 persona_1 = Persona(nombre, 20)
 persona_1.presentation()
+
+nombre2='Fernando'
+trabajador_1 = Trabajador(nombre2,35,'BigData','DataAnalyst')
+trabajador_1.presentation()
